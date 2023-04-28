017c0e41e45c8b06e636c387ba8334f10cbef206
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-28 13:38:56 +0400

    Created button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent
+    TasksComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
