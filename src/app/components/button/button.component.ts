8bbee8b7ffd2acbc914db8fe83d9c3b49bdb849c
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-29 14:18:42 +0400

    Button component works and logs when clicked

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
  
 export class ButtonComponent {
 
-  
+  @Input() btnColor: string = "";
+  @Input() btnText: string = ""
+
+  addTaskBtn() {
+    console.log("Add task button clicked");
+  }
 
 }
