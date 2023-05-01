c98aa6a5beb8a77af0d7b619dc03344e6c081299
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-01 11:59:41 +0400

    Employee list is looped over in new component

--- src/app/components/employee-item/employee-item.component.ts
+++ src/app/components/employee-item/employee-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+// Impport the employee interface
+import { Employee } from 'src/app/models/Employee';
 
 @Component({
   selector: 'app-employee-item',
   templateUrl: './employee-item.component.html',
   styleUrls: ['./employee-item.component.css']
 })
+
+// The component to handle logic for each single employee
 export class EmployeeItemComponent {
+
+  @Input() employee!: Employee;
 
 }
