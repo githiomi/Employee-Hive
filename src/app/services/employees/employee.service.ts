a80afd9b322d20c7a54fa069106072d2d31f948c
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-01 13:42:14 +0400

    Refactored service to call data from JSON server

--- src/app/services/employees/employee.service.ts
+++ src/app/services/employees/employee.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 // Import  the HTTP inbuilt client to make calls
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 // Import the observable to subscribe to
@@ -11,20 +11,35 @@ import { Employee } from '../../models/Employee';
 @Injectable({
   providedIn: 'root'
 })
-export class EmployeeService {
+export class EmployeeService implements OnInit{
 
-  constructor() { }
+  // Server must be running before this can work
+  private employeeUrl:string = 'http://localhost:3000/employees';
+
+  constructor(private httpClient:HttpClient) { }
+
+  ngOnInit(): void {
+    
+    // Initialize the http client
+
+  }
 
   // Method that will return the list of employees
   // getEmployees(): Employee[] {
   //   return Employees;
   // }
 
-  // The same method can be done using observables
-  getEmployees() : Observable<Employee[]> {
-    // To return the observable direclty
-    return of(Employees);
+  // The same method can be done using observables for local data
+  // getEmployees() : Observable<Employee[]> {
+  //   // To return the observable direclty
+  //   return of(Employees);
     
+  // To get the employee data using the http client
+  getEmployees() : Observable<Employee[]> {
+
+    // Use the http client to get data from the given url
+    return this.httpClient.get<Employee[]>(this.employeeUrl);
+
   }
 
 }
