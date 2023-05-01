95d1214b376f969949707abb382b2c2e8eb9575f
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-01 11:42:24 +0400

    Looped through employees and displayed

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+// The mock employee data
+import { Employees } from 'src/app/Mock-Data';
+// The employee interface
+import { Employee } from 'src/app/models/Employee';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
+
+// This is the main employee component
 export class EmployeesComponent {
+
+  // Variable to hold the list of employees
+  employees: Employee[] = Employees;  
 
 }
