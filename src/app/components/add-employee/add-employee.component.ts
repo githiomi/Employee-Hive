3e13ad31fb5e0cb8417dbeb4e55e7904dc21a034
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-02 11:10:24 +0400

    Added form validation

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -16,6 +16,25 @@ export class AddEmployeeComponent {
   isPaid: boolean = false;
 
 
+  // Method called when the form is submitted (ngSubmit)
+  submitForm(){
 
+    // Form validation
+    if (!this.employeeName || !this.employeeAge || !this.employeeGender || !this.employeeEmail || !this.employeePosition){
+      alert("All fields are required");
+      return;
+    }
+
+    // Check for gender
+    if (this.employeeGender.trim().toLowerCase() !== "male" || this.employeeGender.trim().toLowerCase() !== "female"){
+      alert("Gender must be male or female");
+      return;
+    }
+
+    // If form is valid
+    // Create a new employee
+
+
+  }
 
 }
