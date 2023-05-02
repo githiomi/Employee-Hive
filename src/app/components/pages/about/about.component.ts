7e5553abeb2f37586f5f36c1a36f4259d6e35545
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-03 00:06:57 +0400

    Added fa-icon to the about page

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+// Import Font Awesome Icon
+import { faBackward } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+
+  // Set the icon property
+  back = faBackward;
 
 }
