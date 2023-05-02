04c7e695b606da10c9d16db2a61beb606c71eb66
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-02 11:28:07 +0400

    Reset form input to empty after submit

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Employee } from 'src/app/models/Employee';
+import { SEX } from 'src/app/models/SEX';
+
+// Const variable to hold the employee sex
+let sex: SEX;
 
 @Component({
   selector: 'app-add-employee',
@@ -7,9 +12,12 @@ import { Component } from '@angular/core';
 })
 export class AddEmployeeComponent {
 
+  // Data binding
+  @Output() newEmployeeEmitter : EventEmitter<Employee> = new EventEmitter();
+
   // Form input variables
   employeeName!: string;
-  employeeAge!: number;
+  employeeAge!: string;
   employeeGender!: string;
   employeeEmail!: string;
   employeePosition!: string;
@@ -17,23 +25,53 @@ export class AddEmployeeComponent {
 
 
   // Method called when the form is submitted (ngSubmit)
-  submitForm(){
+  submitForm() {
 
     // Form validation
-    if (!this.employeeName || !this.employeeAge || !this.employeeGender || !this.employeeEmail || !this.employeePosition){
+    if (!this.employeeName || !this.employeeAge || !this.employeeGender || !this.employeeEmail || !this.employeePosition) {
       alert("All fields are required");
       return;
     }
 
     // Check for gender
-    if (this.employeeGender.trim().toLowerCase() !== "male" || this.employeeGender.trim().toLowerCase() !== "female"){
-      alert("Gender must be male or female");
-      return;
+    // if (this.employeeGender.trim().toLowerCase() != "male" || this.employeeGender.trim().toLowerCase() != "female") {
+    //   console.log(this.employeeGender);
+    //   alert("Gender must be male or female");
+    //   return;
+    // }
+
+    if (this.employeeGender.trim().toLowerCase() === "male") {
+      sex = SEX.MALE;
+    } else if (this.employeeGender.trim().toLowerCase() === "female") {
+      sex = SEX.FEMALE;
     }
+
+    // Convert age to number
+    const employeeIntAge = Number.parseInt(this.employeeAge);
 
     // If form is valid
     // Create a new employee
+    const newEmployee: Employee = {
+      id: 10,
+      employeeName: this.employeeName,
+      employeeAge: employeeIntAge,
+      employeeGender: sex,
+      employeeEmail: this.employeeEmail,
+      employeePosition: this.employeePosition,
+      isPaid: this.isPaid
+    }
 
+    // We are adding new employees in the parent 'employees' component
+    // Therefore we must emit the new employee
+    this.newEmployeeEmitter.emit(newEmployee);
+
+    // If emission is successful, Clear the form
+    this.employeeName = '';
+    this.employeeAge = '';
+    this.employeeGender = '';
+    this.employeeEmail = '';
+    this.employeePosition = '';
+    this.isPaid = false;
 
   }
 
